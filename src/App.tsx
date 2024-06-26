dd0b091b5dc9fb8f2de77a2a8c02e71ba38f0991
unknown <facuugerez37@gmail.com>
2024-06-26 18:37:15 -0300

    landing-front-done

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import IntroSection from './components/Intro';
+import FeatureSection from './components/Features';
+import WorkflowPage from './components/Workflow';
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
           <Navbar />
           <div className="max-w-7xl mx-auto pt-20 px-6">
             <IntroSection />
+            <FeatureSection />
+            <WorkflowPage />
             <Footer /> 
           </div>
       </div>
--- src/data/index.ts
+++ src/data/index.ts
@@ -1,3 +1,4 @@
+
 export const NavItems = [
   { label: "Nosotros", href: "#" },
   { label: "Trabajos", href: "#" },
@@ -11,3 +12,61 @@ export const FooterItems = [
   { label: "Instagram", href: "#" },
   { label: "Linkedin", href: "#" },
 ]
+
+
+export const features = [
+  {
+    text: "Drag-and-Drop Interface",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia ut delectus dolorem, veniam fugit molestias nesciunt odit dignissimos ratione magnam, tempore dolores perspiciatis asperiores, iusto provident? Quibusdam, iusto. Minus, dolore.",
+  },
+  {
+    text: "Multi-Platform Compatibility",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia ut delectus dolorem, veniam fugit molestias nesciunt odit dignissimos ratione magnam, tempore dolores perspiciatis asperiores, iusto provident? Quibusdam, iusto. Minus, dolore.",
+  },
+  {
+    text: "Built-in Templates",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia ut delectus dolorem, veniam fugit molestias nesciunt odit dignissimos ratione magnam, tempore dolores perspiciatis asperiores, iusto provident? Quibusdam, iusto. Minus, dolore.",
+  },
+  {
+    text: "Real-Time Preview",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia ut delectus dolorem, veniam fugit molestias nesciunt odit dignissimos ratione magnam, tempore dolores perspiciatis asperiores, iusto provident? Quibusdam, iusto. Minus, dolore.",
+  },
+  {
+    text: "Collaboration Tools",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia ut delectus dolorem, veniam fugit molestias nesciunt odit dignissimos ratione magnam, tempore dolores perspiciatis asperiores, iusto provident? Quibusdam, iusto. Minus, dolore.",
+  },
+  {
+    text: "Analytics Dashboard",
+    description:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia ut delectus dolorem, veniam fugit molestias nesciunt odit dignissimos ratione magnam, tempore dolores perspiciatis asperiores, iusto provident? Quibusdam, iusto. Minus, dolore.",
+  },
+];
+
+
+export const checklistItems = [
+  {
+    title: "Code merge made easy",
+    description:
+      "Track the performance of your VR apps and gain insights into user behavior.",
+  },
+  {
+    title: "Review code without worry",
+    description:
+      "Track the performance of your VR apps and gain insights into user behavior.",
+  },
+  {
+    title: "AI Assistance to reduce time",
+    description:
+      "Track the performance of your VR apps and gain insights into user behavior.",
+  },
+  {
+    title: "Share work in minutes",
+    description:
+      "Track the performance of your VR apps and gain insights into user behavior.",
+  },
+];
